6d051de3b957dfd18034d7e3d0fea2515da45fe8
Samuel René <sam@qidigo.com>
2022-05-04 20:30:53 -0400

    Title screne fonctionne et début de la scene lv01

--- ETU/Game/TitleSceneContentManager.h
+++ ETU/Game/TitleSceneContentManager.h
@@ -1,16 +1,19 @@
 #pragma once
 #include "stdafx.h"
-class MenuContentManager
+class TitleSceneContentManager
 {
 public:
-	MenuContentManager();
 	virtual bool loadContent();
-
 	const sf::Texture& getBackgroundTexture() const;
 	const sf::Texture& getMiscTexture() const;
 	const sf::Texture& getTitleScreenTexture() const;
 	const sf::Font& getMainFont() const;
+
+	static TitleSceneContentManager& getInstance();
+
 private:
+	TitleSceneContentManager();
+
 	sf::Texture backgroundTexture;
 	sf::Texture miscTexture;
 	sf::Texture titleScreenTexture;
