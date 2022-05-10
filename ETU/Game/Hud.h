575cba8b529350d979db9fc64f79232d09a686f6
Samuel René <sam@qidigo.com>
2022-05-09 21:01:43 -0400

    Boss working

--- ETU/Game/Hud.h
+++ ETU/Game/Hud.h
@@ -8,26 +8,21 @@ public:
 	void draw(sf::RenderWindow& window)  const;
 	void initialize(const Level01ContentManager& contentManager);
 	void addScoreText();
-	void addNbKillsText();
 	void addNbLivesRemainingText();
 	void addPauseText();
 	void addGameOverText(int score);
 
 	void updateScoreText(int score);
-	void updateNbKillsText(int nbKills);
 	void updateNbLivesText(int nbLivesRemaining);
 	void updatePauseText();
-	void updateGameInfo(int score, int nbKills, int nbLivesRemaining);
+	void updateGameInfo(int score, int nbLivesRemaining);
 
-	void removeMenuStartText();
 	void removeGameInfoText();
 	void removePauseText();
 private:
 	const ContentManager contentManager;
 	sf::View hudView;
-	sf::Text menuStartText;
 	sf::Text scoreText;
-	sf::Text nbKillsText;
 	sf::Text pauseText;
 	sf::Text leaderboardText;
 	sf::Text nbLivesRemainingText;
---
+++ ETU/Game/PlayerStats.h
@@ -0,0 +1,9 @@
+#pragma once
+#define NAME_LENGTH 3
+struct PlayerStats
+{
+  char name[NAME_LENGTH];
+  int nbBonusCollected;
+  int score;
+};
+
